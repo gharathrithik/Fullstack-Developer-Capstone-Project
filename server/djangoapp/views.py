0f92c881924b1e3971cce6be937f47ce6ed1637a
gharathrithik <gharatd424@gmail.com>
2025-11-14 10:06:00 -0500

    Fixed Linting errors

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -59,7 +59,7 @@ def registration(request):
     try:
         User.objects.get(username=username)
         username_exist = True
-    except:
+    except Exception:
         logger.debug("{} is new user".format(username))
 
     if not username_exist:
@@ -131,7 +131,7 @@ def add_review(request):
         try:
             post_review(data)
             return JsonResponse({"status": 200})
-        except:
+        except Exception:
             return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
